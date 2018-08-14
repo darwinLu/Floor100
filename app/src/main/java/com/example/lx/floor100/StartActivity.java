f3e6b6852a289f0d796f718c67cce29b0f765e82
Darwin <Darwin>
2018-08-14 16:54:51 +0800

    实现的功能 +player从平台上掉落 +平台的消失和补充 +跳跃到平台 +添加左右移动平台 +添加上下移动平台 +添加传送带平台 +添加弹簧平台 +给主角施加不同平台上移动和跳跃的效果 +控制主角不能连续起跳 +通过进度条控制主角起跳力量 +加大了平台密度 +单独建立FloorPlatform类 +计算分数并显示 +显示GAMEOVER画面 +调整GAMEOVER画面的显示模式 +调整游戏重新加载时的BUG +添加了音乐和音效 +调整欢迎界面和GAMEOVER界面的显示布局样式 待实现的功能 -优化主角样式和动画 -优化平台样式和动画 -优化平台生成方式和距离 -优化背景样式

--- app/src/main/java/com/example/lx/floor100/StartActivity.java
+++ app/src/main/java/com/example/lx/floor100/StartActivity.java
@@ -8,16 +8,41 @@ import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
+    Button startButton,rankingListButton,optionButton,exitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton = (Button) findViewById(R.id.start_button);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(StartActivity.this,MainActivity.class);
                 startActivity(intent);
+            }
+        });
+        rankingListButton = (Button) findViewById(R.id.ranking_list_button);
+        rankingListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        optionButton = (Button) findViewById(R.id.option_button);
+        optionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        exitButton = (Button) findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
