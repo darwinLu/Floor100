03d79b911f79ca6c4e5a8cf5f20f86396e03111c
Darwin <Darwin>
2018-10-19 13:26:12 +0800

    实现的功能 +player从平台上掉落 +平台的消失和补充 +跳跃到平台 +添加左右移动平台 +添加上下移动平台 +添加传送带平台 +添加弹簧平台 +给主角施加不同平台上移动和跳跃的效果 +控制主角不能连续起跳 +通过进度条控制主角起跳力量 +加大了平台密度 +单独建立FloorPlatform类 +计算分数并显示 +显示GAMEOVER画面 +调整GAMEOVER画面的显示模式 +调整游戏重新加载时的BUG +添加了音乐和音效 +调整欢迎界面和GAMEOVER界面的显示布局样式 +添加蓝天白云背景 +添加普通平台、左右上下移动图片和地面图片 +添加传送带图片并播放动画 +优化主角样式和动画 +解决传送带左右显示不同问题 +添加弹簧平台图片并播放动画 +补传5张图片资源 +云主机部署web服务，建立mysql数据库保存用户成绩 +上传最高分，存入数据库 +排行榜界面，本地加载虚拟排行数据 +设置音乐、音效开关，使用SharedPreference记住设置 +修改标题、按钮颜色，添加背景 +更换app名称和图标 待实现的功能 -从远程数据库获取用户排行 -解决下落距离太大时穿透平台的bug

--- app/src/main/java/com/example/lx/floor100/StartActivity.java
+++ app/src/main/java/com/example/lx/floor100/StartActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
